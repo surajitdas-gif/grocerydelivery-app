f00e91916d22256bdd1ab812c9a46bb0d6dcf141
surajitdas-gif <dasantu0118@gmail.com>
2026-05-24 21:51:22 +0530

    Move Cloudinary secrets to env

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,9 +1,27 @@
-const cloudinary = require("cloudinary").v2;
+// const cloudinary = require("cloudinary").v2;
+
+// cloudinary.config({
+//   cloud_name: "dijegju50",
+//   api_key: "358366729127489",
+//   api_secret: "Au0sLpPZZ16lbGxzSJP_cVnLzeM",
+// });
+
+// module.exports = cloudinary;
+const cloudinary =
+require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "dijegju50",
-  api_key: "358366729127489",
-  api_secret: "Au0sLpPZZ16lbGxzSJP_cVnLzeM",
+
+cloud_name:
+process.env.CLOUDINARY_CLOUD_NAME,
+
+api_key:
+process.env.CLOUDINARY_API_KEY,
+
+api_secret:
+process.env.CLOUDINARY_API_SECRET,
+
 });
 
-module.exports = cloudinary;
+module.exports =
+cloudinary;
